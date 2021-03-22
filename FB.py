c30c902c36914665776a2cae8a0bc064e2d22834
Pival <pival1302@gmail.com>
2021-03-22 12:08:01 +0100

    Merge Forging Bonds Revival into Forging Bonds

--- FB.py
+++ FB.py
@@ -11,6 +11,8 @@ from scenario import Conversation, StoryNavBar
 from reward import parseReward
 from Reverse import reverseForgingBonds
 
+from wikiUtil import exportPage
+
 COLORS = ['Red', 'Orange', 'Green', 'Blue']
 
 def FBInfobox(data):
@@ -60,20 +62,60 @@ def FBConversation(data):
     s += "\n" + StoryNavBar(util.getName(data['title']), "", "")
     return s
 
+def ForgingBondsOriginal(data):
+    if data["id_tag"] != data["original_id_tag"]:
+        raise ValueError("Given Forging Bonds is a revival")
+    s = FBInfobox(data) + "\n"
+    s += mapUtil.Availability(data['event_avail'], f"Forging Bonds: {util.getName(data['title'])} (Notification)", "[[Forging Bonds]] event") + "\n"
+    s += FBRewards(data) + "\n"
+    s += FBConversation(data) + "\n"
+    s += mapUtil.InOtherLanguage(data['title'])
+    s += "{{Main Events Navbox}}"
+    content[util.getName(data['title'])] = s
+    return (util.getName(data['title']), content)
+
+def ForgingBondsRevival(data):
+    if data["id_tag"] == data["original_id_tag"]:
+        raise ValueError("Given Forging Bonds is not a revival")
+
+    data["title"] = data["title"].replace(data['id_tag'], data['original_id_tag'])
+    name = util.getName(data["title"])
+    page = getPageContent([name])[name]
+
+    start = data["event_avail"]["start"]
+    end = util.timeDiff(data["event_avail"]["finish"])
+    wikiname = util.cleanStr(f"{name} {datetime.strptime(data['event_avail']['start'], util.TIME_FORMAT).strftime('%Y%m%d')}")
+    rewards = FBRewards(data).replace("First Summon Ticket I", "First Summon Ticket II").replace("==Rewards==\n", "")
+
+    if page.find(start) == -1:
+        page = re.sub(r"(\|startTime=.*)\n", f"\\1;{start}\n", page)
+    if page.find(end) == -1:
+        page = re.sub(r"(\|endTime=.*)\n", f"\\1;{end}\n", page)
+    if page.count("(Notification)") == 1:
+        page = re.sub(r"((?:\*\s*\{\{\s*HT\s*\|.*\n)+)", f"\\1* {{{{HT|{start}}}}} – {{{{HT|{end}}}}} ([[Forging Bonds Revival: {name} (Notification)|Notification]])\n", page)
+    if not re.search(r"===\s*Original [rR]un\s*===", page):
+        page = re.sub("(==\s*Rewards\s*==\n)", "\\1===Original run===\n", page)
+    if not re.search(f"wikiname\\s*=\\s*{wikiname}", page):
+        page = re.sub(r"\}\}\n(\n*==\s*Special [cC]onversations\s*==)", "}}\n===Rerun===\n"+rewards+"\\1", page)
+    
+    return (name, content)
+
 def ForgingBonds(tagId: str) -> dict:
     datas = reverseForgingBonds(tagId)
     content = {}
     for data in datas:
-        if data["id_tag"] != data["original_id_tag"]:
-            print("Revival.")
+        if data["id_tag"] == data["original_id_tag"]:
+            name, page = ForgingBondsOriginal(data)
         else:
-            s = FBInfobox(data) + "\n"
-            s += mapUtil.Availability(data['event_avail'], f"Forging Bonds: {util.getName(data['title'])} (Notification)", "[[Forging Bonds]] event") + "\n"
-            s += FBRewards(data) + "\n"
-            s += FBConversation(data) + "\n"
-            s += mapUtil.InOtherLanguage(data['title'])
-            s += "{{Main Events Navbox}}"
-            content[util.getName(data['title'])] = s
+            name, page = ForgingBondsRevival(data)
+        content[name] = page
+    return content
+
+def exportForgingBonds(tagId: str):
+    content = ForgingBonds(tagId)
+    for name in content:
+        isRevival = True if re.search(r"===\s*Rerun\s*===", content[name]) else False
+        exportPage(name, content[name], 'Bot: Forging Bonds revival' if isRevival else 'Bot: new Forging Bonds', create=not isRevival)
     return content
 
 
